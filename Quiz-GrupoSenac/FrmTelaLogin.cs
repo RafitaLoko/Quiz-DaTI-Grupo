b0c8e8356522cb34a95e78651126d150940e37e8
RAFAEL DE CARVALHO WENDLAND <04311084048@senacrs.edu.br>
2026-08-13 19:59:01 -0300

    ve se foi

---
+++ Quiz-GrupoSenac/FrmTelaLogin.cs
@@ -0,0 +1,15 @@
+namespace Quiz_GrupoSenac
+{
+    public partial class FrmTelaLogin : Form
+    {
+        public FrmTelaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmTelaLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
