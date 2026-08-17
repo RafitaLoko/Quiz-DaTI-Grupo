f13fcc9ac91db1f70820225a08836bc5869445d8
RAFAEL DE CARVALHO WENDLAND <04311084048@senacrs.edu.br>
2026-08-17 19:48:22 -0300

    Historico

---
+++ Quiz-GrupoSenac/FrmTelaHistorico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_GrupoSenac
+{
+    public partial class FrmTelaHistorico : Form
+    {
+        public FrmTelaHistorico()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvHistorico_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
