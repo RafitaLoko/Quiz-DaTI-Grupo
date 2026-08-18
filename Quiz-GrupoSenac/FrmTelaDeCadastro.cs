39c5949dbac971c22dac915988040a3c86975e89
RAFAEL DE CARVALHO WENDLAND <04311084048@senacrs.edu.br>
2026-08-18 19:20:16 -0300

    Cancelar botaao

--- Quiz-GrupoSenac/FrmTelaDeCadastro.cs
+++ Quiz-GrupoSenac/FrmTelaDeCadastro.cs
@@ -24,7 +24,9 @@ namespace Quiz_GrupoSenac
 
         private void btnCancelarTelaCadastro_Click(object sender, EventArgs e)
         {
-
+            this.Hide();
+            FrmTelaLogin frmTelaLogin = new FrmTelaLogin();
+            frmTelaLogin.ShowDialog();
         }
 
         private void FrmTelaDeCadastro_Load(object sender, EventArgs e)
