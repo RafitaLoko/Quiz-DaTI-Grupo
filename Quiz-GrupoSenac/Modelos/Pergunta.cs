bbde7ef8fc1f165a79ec571706673a5646ae29d6
LUCAS ARIEL DOS SANTOS <05297679028@senacrs.edu.br>
2026-08-17 21:46:39 -0300

    Mais One

---
+++ Quiz-GrupoSenac/Modelos/Pergunta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_GrupoSenac.Modelos
+{
+    public class Pergunta
+    {
+        public int Id { get; set; }
+
+        public string Enunciado { get; set; }
+
+        public string Tipo { get; set; }
+
+        public string Nivel { get; set; }
+
+        public string Tema { get; set; }
+
+        public int Pontuacao { get; set; }
+
+        public List<Alternativa> alternativas { get; set; }
+
+
+    }
+}
