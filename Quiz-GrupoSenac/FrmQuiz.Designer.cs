ce5b38cfa8140d4de34f1c960cd0852edb12c5d3
RAFAEL DE CARVALHO WENDLAND <04311084048@senacrs.edu.br>
2026-08-18 19:54:56 -0300

    Alterações

--- Quiz-GrupoSenac/FrmQuiz.Designer.cs
+++ Quiz-GrupoSenac/FrmQuiz.Designer.cs
@@ -44,6 +44,7 @@
             // lblNickQuiz
             // 
             lblNickQuiz.AutoSize = true;
+            lblNickQuiz.BackColor = Color.Yellow;
             lblNickQuiz.Location = new Point(12, 9);
             lblNickQuiz.Name = "lblNickQuiz";
             lblNickQuiz.Size = new Size(46, 25);
@@ -53,6 +54,7 @@
             // lblNivelQuiz
             // 
             lblNivelQuiz.AutoSize = true;
+            lblNivelQuiz.BackColor = Color.Yellow;
             lblNivelQuiz.Location = new Point(645, 9);
             lblNivelQuiz.Name = "lblNivelQuiz";
             lblNivelQuiz.Size = new Size(55, 25);
@@ -62,6 +64,7 @@
             // lblValorQuiz
             // 
             lblValorQuiz.AutoSize = true;
+            lblValorQuiz.BackColor = Color.Yellow;
             lblValorQuiz.Location = new Point(12, 416);
             lblValorQuiz.Name = "lblValorQuiz";
             lblValorQuiz.Size = new Size(56, 25);
@@ -84,6 +87,7 @@
             // lblPerguntaQuiz
             // 
             lblPerguntaQuiz.AutoSize = true;
+            lblPerguntaQuiz.BackColor = Color.GreenYellow;
             lblPerguntaQuiz.Location = new Point(64, 66);
             lblPerguntaQuiz.Name = "lblPerguntaQuiz";
             lblPerguntaQuiz.Size = new Size(86, 25);
@@ -93,50 +97,55 @@
             // rbResposta1
             // 
             rbResposta1.AutoSize = true;
+            rbResposta1.BackColor = Color.MintCream;
             rbResposta1.Location = new Point(41, 218);
             rbResposta1.Name = "rbResposta1";
             rbResposta1.Size = new Size(124, 29);
             rbResposta1.TabIndex = 5;
             rbResposta1.TabStop = true;
             rbResposta1.Text = "Resposta 1";
-            rbResposta1.UseVisualStyleBackColor = true;
+            rbResposta1.UseVisualStyleBackColor = false;
             // 
             // rbResposta2
             // 
             rbResposta2.AutoSize = true;
-            rbResposta2.Location = new Point(41, 331);
+            rbResposta2.BackColor = Color.MintCream;
+            rbResposta2.Location = new Point(41, 322);
             rbResposta2.Name = "rbResposta2";
             rbResposta2.Size = new Size(120, 29);
             rbResposta2.TabIndex = 6;
             rbResposta2.TabStop = true;
             rbResposta2.Text = "resposta 2";
-            rbResposta2.UseVisualStyleBackColor = true;
+            rbResposta2.UseVisualStyleBackColor = false;
             // 
             // rbResposta3
             // 
             rbResposta3.AutoSize = true;
+            rbResposta3.BackColor = Color.MintCream;
             rbResposta3.Location = new Point(404, 218);
             rbResposta3.Name = "rbResposta3";
             rbResposta3.Size = new Size(120, 29);
             rbResposta3.TabIndex = 7;
             rbResposta3.TabStop = true;
             rbResposta3.Text = "resposta 3";
-            rbResposta3.UseVisualStyleBackColor = true;
+            rbResposta3.UseVisualStyleBackColor = false;
             // 
             // rbResposta4
             // 
             rbResposta4.AutoSize = true;
-            rbResposta4.Location = new Point(404, 331);
+            rbResposta4.BackColor = Color.MintCream;
+            rbResposta4.Location = new Point(404, 322);
             rbResposta4.Name = "rbResposta4";
             rbResposta4.Size = new Size(124, 29);
             rbResposta4.TabIndex = 8;
             rbResposta4.TabStop = true;
             rbResposta4.Text = "Resposta 4";
-            rbResposta4.UseVisualStyleBackColor = true;
+            rbResposta4.UseVisualStyleBackColor = false;
             // 
             // lblPerguntaNumeroQuiz
             // 
             lblPerguntaNumeroQuiz.AutoSize = true;
+            lblPerguntaNumeroQuiz.BackColor = Color.LightGoldenrodYellow;
             lblPerguntaNumeroQuiz.Location = new Point(373, 8);
             lblPerguntaNumeroQuiz.Name = "lblPerguntaNumeroQuiz";
             lblPerguntaNumeroQuiz.Size = new Size(155, 25);
