88beb07e55df980a2fccaef3e4c4922d031216d4
LUCAS ARIEL DOS SANTOS <05297679028@senacrs.edu.br>
2026-08-24 21:34:51 -0300

    nova classe alternaiva

---
+++ Quiz-GrupoSenac/Repositories/AlternativaRepository.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using Quiz_GrupoSenac.Banco;
+using Quiz_GrupoSenac.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_GrupoSenac.Repositories
+{
+    public class AlternativaRepository
+    {
+        private static Conexao conexao = new Conexao();
+
+        public static async Task Cadastrar(Alternativa alternativa)
+        {
+            await conexao.Conectar().QueryAsync(
+
+                @"    INSERT INTO ( PerguntaId, Texto, Correta) 
+                        VALUES ( @PerguntaId, @Texto, @Coreta)
+
+              ",
+                alternativa
+                );
+        }
+
+        public static async Task<Alternativa> BuscarPergunta( int perguntaId)
+        {
+            var alternativa = await conexao.Conectar().QueryFirstOrDefaultAsync<Alternativa>(
+            @"
+                SELECT 
+                    Id,
+                    PerguntaId,
+                    Texto,
+                    Correta
+                FROM
+                    Alternativa
+                WHERE
+                    PerguntaId = @PerguntaId
+
+            ",
+            new
+            {
+                PerguntaId = perguntaId
+            }
+            );
+            return alternativa;
+        }
+
+
+    }
+}
