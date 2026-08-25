5342a557b129974bfbdda9d5fe5a0c92e00798f0
MIKAEL KAUE LOPES PAIM <05286359051@senacrs.edu.br>
2026-08-24 21:54:59 -0300

    alteracoes

---
+++ Quiz-GrupoSenac/Repositories/UsuarioRankingRepository.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using Quiz_GrupoSenac.Banco;
+using Quiz_GrupoSenac.Modelos;
+
+
+
+namespace Quiz_GrupoSenac.Repositories
+{
+    public class UsuarioRankingRepository
+    {
+        private static Conexao Conexao = new Conexao();
+        public static async Task<IEnumerable<UsuarioRanking>> ObterTodos()
+        {
+            var UsuarioRanking = await Conexao.Conectar().QueryAsync<UsuarioRanking>();
+                
+        }
+    }
+}
+
