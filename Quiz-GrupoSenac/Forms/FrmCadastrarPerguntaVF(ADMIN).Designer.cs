343e72d222e7e57c1d2144862882f7eff74113c1
MIKAEL KAUE LOPES PAIM <05286359051@senacrs.edu.br>
2026-09-03 21:39:46 -0300

    adiciona cor para botoes

--- Quiz-GrupoSenac/Forms/FrmCadastrarPerguntaVF(ADMIN).Designer.cs
+++ Quiz-GrupoSenac/Forms/FrmCadastrarPerguntaVF(ADMIN).Designer.cs
@@ -280,42 +280,46 @@
             // 
             // button1
             // 
+            button1.BackColor = SystemColors.ActiveCaption;
             button1.Location = new Point(424, 306);
             button1.Name = "button1";
             button1.Size = new Size(112, 34);
             button1.TabIndex = 64;
             button1.Text = "1";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // button2
             // 
+            button2.BackColor = SystemColors.ActiveCaption;
             button2.Location = new Point(608, 306);
             button2.Name = "button2";
             button2.Size = new Size(112, 34);
             button2.TabIndex = 65;
             button2.Text = "2";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
             // 
             // button3
             // 
+            button3.BackColor = SystemColors.ActiveCaption;
             button3.Location = new Point(425, 373);
             button3.Name = "button3";
             button3.Size = new Size(112, 34);
             button3.TabIndex = 66;
             button3.Text = "3";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
             // 
             // button4
             // 
+            button4.BackColor = SystemColors.ActiveCaption;
             button4.Location = new Point(608, 373);
             button4.Name = "button4";
             button4.Size = new Size(112, 34);
             button4.TabIndex = 67;
             button4.Text = "4";
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
             button4.Click += button4_Click;
             // 
             // label3
